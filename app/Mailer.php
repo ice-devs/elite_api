f05014f039b2bb78c989dd99087b668a404ccd30
markvinnie <othukeotubu@gmail.com>
2025-09-27 23:56:31 +0100

    Mail fix

--- app/Mailer.php
+++ app/Mailer.php
@@ -89,7 +89,7 @@ class Mailer
                 'name'           => $request->input('name'),
                 'status'         => $request->input('status'),
                 'payMethod'      => $request->input('payMethod'),
-                'products'       => (object) $productJson,
+                'products'       => json_decode($productJson),
                 'total'          => $request->input('total'),
                 'amountPaid'     => $request->input('amountPaid'),
                 'balance'        => $request->input('balance'),
